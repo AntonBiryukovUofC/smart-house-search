7e3483e9b4444a785faf1e25f418f5423f126ef2
jjedd97 <jedonnelly97@yahoo.ca>
2021-05-06 15:22:46 -0600

    cleanup

--- redis_locations.py
+++ redis_locations.py
@@ -8,5 +8,4 @@ from location import geocode_destination_here, Location
 def location_from_listing(listing: str, redis: Redis):
     value=redis.get(listing)
     value=json.loads((value))
-    print(value)
     return Location(geocode_destination_here(value['address']))

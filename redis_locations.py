8791028a6904494cabeae195faecbd7c5adf91d1
jjedd97 <jedonnelly97@yahoo.ca>
2021-05-06 15:20:20 -0600

    Add helper file for redis to deal with locations

---
+++ redis_locations.py
@@ -0,0 +1,12 @@
+import json
+
+from redis import Redis
+
+from location import geocode_destination_here, Location
+
+
+def location_from_listing(listing: str, redis: Redis):
+    value=redis.get(listing)
+    value=json.loads((value))
+    print(value)
+    return Location(geocode_destination_here(value['address']))

449a391f09abae7cf32f55a85a9d9d4f504ace4b
jjedd97 <jedonnelly97@yahoo.ca>
2021-05-06 16:07:19 -0600

    inverse lookup

--- redis_locations.py
+++ redis_locations.py
@@ -6,6 +6,14 @@ from location import geocode_destination_here, Location
 
 
 def location_from_listing(listing: str, redis: Redis):
-    value=redis.get(listing)
-    value=json.loads((value))
-    return Location(geocode_destination_here(value['address']))
+    value = redis.get(listing)
+    value = json.loads((value))
+    l = Location(geocode_destination_here(value['address']))
+    l.listing_key = listing
+    return l
+
+
+def listing_from_location(location: Location, redis: Redis):
+    # not all locations are a listing
+    return json.loads(redis.get(location.listing_key))
+
